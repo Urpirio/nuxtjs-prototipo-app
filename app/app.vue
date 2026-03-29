8bf378281f43c423285750132bbee373c52960a7
UrpirioDev <urpiriojunior@gmail.com>
2026-03-28 20:16:02 -0400

    add initial app layout with Header and Footer components

---
+++ app/app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Footer from "./components/cps - globales/Footer.vue";
+import Header from "./components/cps - globales/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <NuxtPage />
+  <Footer />
+</template>
